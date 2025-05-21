9bf49a21903275f5306c87370f0a89fd598d8f85
mehran9876 <mehranghari9876@gmail.com>
2025-05-21 10:58:00 +0330

    feat: built a react quiz using usReducer

---
+++ src/components/FinisheScreen.jsx
@@ -0,0 +1,28 @@
+function FinisheScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (100 * points) / maxPossiblePoints;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({percentage.toFixed(2)}%)
+      </p>
+      <p className="highscore">(highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
+    </>
+  );
+}
+
+export default FinisheScreen;
---
+++ src/components/NextButton.jsx
@@ -0,0 +1,26 @@
+function NextButton({ dispatch, answer, index, numQuestions }) {
+  if (answer === null) return;
+
+  if (index < numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        next
+      </button>
+    );
+  }
+  if (index === numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+  }
+}
+
+export default NextButton;
